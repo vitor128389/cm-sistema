f0b224fcfcd02579eab264750b02d32f918dfd30
vitor128389 <vitormendonca128@gmail.com>
2026-09-10 21:19:07 -0300

    remove: categoria Moveis Deposito nao existe mais

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -7,7 +7,7 @@ import { useLoja } from "@/contexts/LojaContext";
 import { carregarProdutosComEstoque } from "@/lib/produtos";
 import type { ProdutoComVariantes } from "@/types";
 
-const CATEGORIAS_FIXAS = ["Sofás", "Poltronas", "Namoradeiras", "Puffs", "Camas", "Cabeceiras", "Móveis Depósito"];
+const CATEGORIAS_FIXAS = ["Sofás", "Poltronas", "Namoradeiras", "Puffs", "Camas", "Cabeceiras"];
 
 export default function ProdutosPage() {
   const { lojaAtual } = useLoja();
