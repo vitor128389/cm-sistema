ebb752617669433a2d3224e5f51177b6124dff72
vitor128389 <vitormendonca128@gmail.com>
2026-09-12 10:15:03 -0300

    fix: remove categoria Puffs vazia da lista fixa (produtos ja fundidos em Cadeiras e Puffs)

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -7,7 +7,7 @@ import { useLoja } from "@/contexts/LojaContext";
 import { carregarProdutosComEstoque } from "@/lib/produtos";
 import type { ProdutoComVariantes } from "@/types";
 
-const CATEGORIAS_FIXAS = ["Sofás", "Poltronas e Namoradeiras", "Puffs", "Cama e Colchões", "Cabeceiras e Baús"];
+const CATEGORIAS_FIXAS = ["Sofás", "Poltronas e Namoradeiras", "Cama e Colchões", "Cabeceiras e Baús"];
 
 export default function ProdutosPage() {
   const { lojaAtual } = useLoja();
